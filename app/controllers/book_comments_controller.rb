d3241c7fd7bab388017ff923bf62fa7947707b85
einbone921 <einbone921@gmail.com>
2020-12-09 00:09:16 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,14 +6,15 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		@book_comment.save
+		unless @book_comment.save
+		  render 'errors'
+		end
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
   		book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
 	end
 
 	private
